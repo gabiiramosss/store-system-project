bb5454bfb6e18499e17e7347c31a3c4f1c78dc2a
Gabriela Rosa <gabii.ramos1309@gmail.com>
2023-04-28 11:06:00 -0300

    Início da program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Sistema de pagamentos
+
+static char menu()
+{
+    Console.WriteLine($"---------------------------------------------");
+    Console.WriteLine($"¦           Sistema de Pagamentos           ¦");
+    Console.WriteLine($"---------------------------------------------");
+
+    Console.WriteLine($"Informe o valor da compra: ");
+    float valor = float.Parse(Console.ReadLine());
+
+    Console.WriteLine(@$"
+    Qual a forma de pagamento?
+    1 - Cartão de crédito
+    2 - Cartão de débito
+    3 - Boleto
+    4 - Cancelar operação
+
+    0 - Sair
+    ");
+    return char.Parse(Console.ReadLine());
+}
+
+do
+{
+    switch (menu())
+    {
+        case '1':
+            Console.WriteLine($"Deseja parcelar? Digite 's' ou 'n'.");
+            char resposta = char.Parse(Console.ReadLine());
+
+            if (resposta == 's')
+            {
+
+            }
+            else
+            {
+
+            }
+            break;
+
+        case '2':
+            break;
+
+        case '3':
+            //desconto de 12%
+            //exibir código de barras
+            break;
+    }
+} while (menu() != '0');
